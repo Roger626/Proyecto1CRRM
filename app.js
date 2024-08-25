4bf84ed6e97ac1133bca9ad811abb96174eb93fe
Crei03 <carlosreina2100@gmail.com>
2024-08-25 11:35:00 -0500

    primer

---
+++ app.js
@@ -0,0 +1,16 @@
+function validarSoloLetras(input) {
+    // Expresión regular que permite solo letras (mayúsculas y minúsculas) y espacios
+    var soloLetras = /^[a-zA-Z\s]*$/;
+
+    // Verifica si el valor del input coincide con la expresión regular
+    if (!soloLetras.test(input.value)) {
+        // Si no coincide, muestra un mensaje de error
+        document.getElementById("msgError").style.display = "block";
+        
+        // Elimina el último carácter ingresado que no es válido
+        input.value = input.value.slice(0, -2);
+    } else {
+        // Si todo está bien, oculta el mensaje de error
+        document.getElementById("msgError").style.display = "none";
+    }
+}
