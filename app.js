53745ba92f5a9ed15ae09a1b94298788fd976dd5
Carlos <carlosreina2100@gmail.com>
2024-09-03 15:36:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function validarSoloLetras(input) {
+function soloLetras(input) {
     // Expresión regular que permite solo letras (mayúsculas y minúsculas) y espacios
     var soloLetras = /^[a-zA-Z\s]*$/;
 
@@ -8,9 +8,29 @@ function validarSoloLetras(input) {
         document.getElementById("msgError").style.display = "block";
         
         // Elimina el último carácter ingresado que no es válido
-        input.value = input.value.slice(0, -2);
+        input.value = input.value.slice(0,-1);
     } else {
         // Si todo está bien, oculta el mensaje de error
         document.getElementById("msgError").style.display = "none";
     }
 }
+
+function soloNumeros(input){
+    var soloNumeros = /^\d+$/;
+
+    if(!soloNumeros.test(input.value)){
+        input.value = input.value.slice(0,-1);
+    } 
+}
+
+function impuestoRenta(input){
+    sueldo = document.getElementById("sueldo").value;
+    console.log("sueldo: " + sueldo);
+
+    impuesto = sueldo*13;
+    
+    if (impuesto ? 0 : 11000){
+        console.log("no paga")
+    }
+}
+
