5d61e8666c37b7ec82ff0af72f76813b98fd55ab
Crei03 <carlosreina2100@gmail.com>
2024-09-05 15:23:14 -0500

    ..

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
-function soloLetras(input) {
-    // Expresión regular que permite solo letras (mayúsculas y minúsculas) y espacios
-    var soloLetras = /^[a-zA-Z\s]*$/;
+$(function() {
 
-    // Verifica si el valor del input coincide con la expresión regular
-    if (!soloLetras.test(input.value)) {
-        // Si no coincide, muestra un mensaje de error
-        document.getElementById("msgError").style.display = "block";
+    $(".salario").on("input", function(){
+
+        horasTrabajas = document.getElementById("horas").value;
+        salarioXHora = document.getElementById("salarioHora").value;
         
-        // Elimina el último carácter ingresado que no es válido
-        input.value = input.value.slice(0,-1);
-    } else {
-        // Si todo está bien, oculta el mensaje de error
-        document.getElementById("msgError").style.display = "none";
-    }
-}
-
-function soloNumeros(input){
-    var soloNumeros = /^\d+$/;
-
-    if(!soloNumeros.test(input.value)){
-        input.value = input.value.slice(0,-1);
-    } 
-}
-
-function impuestoRenta(input){
-    sueldo = document.getElementById("sueldo").value;
-    console.log("sueldo: " + sueldo);
-
-    impuesto = sueldo*13;
+        var sBruto = horasTrabajas * salarioXHora;
     
-    if (impuesto ? 0 : 11000){
-        console.log("no paga")
-    }
-}
+        document.getElementById("sBruto").value = sBruto;
+    
+        calcularSalarioNeto();
 
+    });
+
+
+
+
+    function calcularSalarioNeto(){
+        var sBruto = document.getElementById("sBruto").value;
+    
+        var i = sBruto * 0.0975;
+        const seguroSocial = parseFloat(i.toFixed(2));
+        document.getElementById("sSocial").value = seguroSocial;   
+        
+        var i = sBruto * 0.0125;
+        const seguroEducativo = parseFloat(i.toFixed(2));
+        document.getElementById("sEducativo").value = seguroEducativo;
+        
+        var iR = sBruto *12 ;
+        let impuestoRenta = 0; // Declarar impuestoRenta fuera del ámbito de los bloques if/else if
+        
+        if(iR > 50000){
+            var i = iR * 0.25;
+            const impuestoRenta = parseFloat(i.toFixed(2));
+            var mensual = impuestoRenta / 12;
+            document.getElementById("iRenta").value = mensual.toFixed(2);
+            console.log("Paga 25%");
+        } else{
+            var i = iR * 0.15;
+            var mensual = impuestoRenta / 12;
+            document.getElementById("iRenta").value = mensual.toFixed(2);
+            console.log("Paga 15%");
+        }
+
+        const sNeto = sBruto - (seguroSocial + seguroEducativo);
+        document.getElementById("sNeto").value = sNeto.toFixed(2); // Convertir sNeto a decimal con 2 dígitos
+    }
+
+});
